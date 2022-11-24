a32867a24dc698179f4e93aa3d0c3b382aa74576
Tushar Mahajan <tu.mahajan@gemsol.in>
2022-11-24 13:43:03 +0530

    First Project

---
+++ out/test/resources/Features/Perfecto.feature
@@ -0,0 +1,8 @@
+@test123
+Feature: Search using Appium Inspector
+
+  Background:
+    Given Launch the application
+
+  Scenario: check launch of application
+      When Check the application
---
+++ src/test/java/StepDefinition/DemoApp.java
@@ -0,0 +1,49 @@
+package StepDefinition;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DemoApp {
+
+
+@Managed(driver = "appium")
+    WebDriver driver;
+
+
+@Given("^Launch the application$")
+public void app() throws MalformedURLException, InterruptedException {
+
+    Thread.sleep(2000);
+    DesiredCapabilities cap = new DesiredCapabilities();
+
+    cap.setCapability("deviceName", "Pixel3");
+
+    cap.setCapability("udid", "emulator-5554");
+
+    cap.setCapability("platformName", "Android");
+
+    cap.setCapability("app","C:\\Users\\tu.mahajan\\AppData\\Local\\Android\\Sdk\\platform-tools\\ExpenseAppVer1.0.apk");
+
+    driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+
+    System.out.println("Launched application");
+
+    Thread.sleep(6000);
+
+}
+@When("^Check the application$")
+public void check()
+{
+    System.out.println("Application is launched");
+}
+
+}
