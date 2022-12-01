a960f3ab2e7294364e93762c6a7658bc9affb2cc
gem-shubhambajaj <103281079+gem-shubhambajaj@users.noreply.github.com>
2022-12-02 00:35:12 +0530

    Drag And Toast Verify

---
+++ Assignment-6/src/test/java/UiPage/locator.java
@@ -0,0 +1,24 @@
+package UiPage;
+
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+
+public class locator {
+
+    public static By pref = AppiumBy.accessibilityId("Preference");
+    public static By option_app = AppiumBy.accessibilityId("App");
+    public static By option_views = AppiumBy.accessibilityId("Views");
+    public static By dragDrop = AppiumBy.accessibilityId("Drag and Drop");
+    public static By expandable_lists = AppiumBy.accessibilityId("Expandable Lists");
+    public static By custom_adapt = AppiumBy.accessibilityId("1. Custom Adapter");
+    public static By alert_dialogue = AppiumBy.accessibilityId("Alert Dialogs");
+    public static By alert_firstOption = By.id("io.appium.android.apis:id/two_buttons");
+    public static By ok_btn = By.id("android:id/button1");
+    public static By prefDepend = AppiumBy.accessibilityId("3. Preference dependencies");
+    public static By wifiCheckBox=AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.RelativeLayout");
+    public static By longpress=AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ExpandableListView/android.widget.TextView[1]");
+    public static By toastMsg = AppiumBy.xpath("//android.widget.Toast[1]");
+    public static By wifiSetting =AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[2]/android.widget.RelativeLayout");
+    public static By sourceDrag = AppiumBy.id("io.appium.android.apis:id/drag_dot_3");
+    public static By dragResult = AppiumBy.id("io.appium.android.apis:id/drag_result_text");
+}
